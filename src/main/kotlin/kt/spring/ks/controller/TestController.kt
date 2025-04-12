8c5f4cde54e3e33b3bb65305b36ea71c7c822f6d
vsemenyug <vsemenyug@gmail.com>
2025-04-12 15:34:13 +0200

    vid2 - test controller added

---
+++ src/main/kotlin/kt/spring/ks/controller/TestController.kt
@@ -0,0 +1,13 @@
+package kt.spring.ks.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class TestController {
+
+    @GetMapping("/test")
+    fun test(): String = "test"
+}
