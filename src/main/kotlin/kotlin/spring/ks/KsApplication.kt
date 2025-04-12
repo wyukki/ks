48063a79af80dfe390a322214fcc63559f9bc46b
vsemenyug <vsemenyug@gmail.com>
2025-04-12 15:04:16 +0200

    Init Kotlin/Spring Boot project

---
+++ src/main/kotlin/kotlin/spring/ks/KsApplication.kt
@@ -0,0 +1,11 @@
+package kotlin.spring.ks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KsApplication
+
+fun main(args: Array<String>) {
+	runApplication<KsApplication>(*args)
+}
---
+++ src/test/kotlin/kotlin/spring/ks/KsApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlin.spring.ks
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
