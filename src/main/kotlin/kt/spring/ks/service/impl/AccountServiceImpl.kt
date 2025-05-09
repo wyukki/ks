2412433ccd62674ad7dc60391c6d5efdc6d70fc3
Volodymyr Semenyug <71630483+wyukki@users.noreply.github.com>
2025-05-09 18:04:50 +0200

    vid8 - advice added, new tests (#6)

--- src/main/kotlin/kt/spring/ks/service/impl/AccountServiceImpl.kt
+++ src/main/kotlin/kt/spring/ks/service/impl/AccountServiceImpl.kt
@@ -1,13 +1,15 @@
 package kt.spring.ks.service.impl
 
 import kt.spring.ks.dto.AccountDto
+import kt.spring.ks.exception.NotFoundException
 import kt.spring.ks.mapper.AccountServiceMapper
 import kt.spring.ks.repository.AccountRepository
 import kt.spring.ks.service.AccountService
 import org.springframework.stereotype.Service
 
 @Service
-class AccountServiceImpl(private val repository: AccountRepository, private val mapper: AccountServiceMapper) : AccountService {
+class AccountServiceImpl(private val repository: AccountRepository, private val mapper: AccountServiceMapper) :
+    AccountService {
 
     override fun getAccounts(): Collection<AccountDto> {
         val accounts = repository.getAccounts()
@@ -17,6 +19,6 @@ class AccountServiceImpl(private val repository: AccountRepository, private val 
     override fun getAccount(id: String): AccountDto {
         val account = repository.getAccount(id)
         account?.let { return mapper.toAccountDto(it) }
-        throw RuntimeException("Account with id $id not found")
+            ?: throw NotFoundException("Account with id $id doesn't exist")
     }
 }
